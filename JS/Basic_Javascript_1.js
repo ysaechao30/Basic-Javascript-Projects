b365609956fa2ee926a034b2532d2f4f704bbc72
ysaechao30 <ysaechao30@gmail.com>
2020-02-03 22:59:15 -0800

    Initial commit

---
+++ JS/Basic_Javascript_1.js
@@ -0,0 +1,21 @@
+window.alert("I'm hoping this works");
+
+document.write("\"hi\"");
+
+var B = "Concatenated"+"String"
+document.write(B)
+
+
+var Dogs = Dan="Weiner", Kurt="Mastiff", Brent="Chihuahua"
+document.write(Dogs)
+
+var Kurt=Kurt.fontcolor("green");
+document.write(Kurt)
+
+var Dan=Dan.fontcolor("red");
+document.write(Dan)
+
+var Brent=Brent.fontcolor("blue");
+document.write(Brent) 
+
+document.write(3+5);
