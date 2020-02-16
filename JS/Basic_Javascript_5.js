0730171fc55e78578e94089d2872db7f00446c30
ysaechao30 <ysaechao30@gmail.com>
2020-02-15 18:34:17 -0800

    update boolean
    Boolean

---
+++ JS/Basic_Javascript_5.js
@@ -0,0 +1,3 @@
+function not_Function() {
+    document.getElementById("Not").innerHTML=!(5>10);
+}
---
+++ JS/NaN.js
@@ -0,0 +1,15 @@
+function my_Function() {
+    document.getElementById("Test").innerHTML= 0/0;
+}
+
+function my_Function1() {
+    document.getElementById("Test 1").innerHTML= isNaN ('This is a string');
+}
+
+function my_Function2() {
+    document.getElementById("Test 2").innerHTML= isNaN('007');
+}
+
+document.write(2E310);
+
+document.write(-32E310);
