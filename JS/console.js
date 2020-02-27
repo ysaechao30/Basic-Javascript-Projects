8e3665845886189240b059bb4f4c3c94a798b052
ysaechao30 <ysaechao30@gmail.com>
2020-02-26 16:34:19 -0800

    updated
    added separate figures
    console log, global var, and local var each separate files.

---
+++ JS/console.js
@@ -0,0 +1,9 @@
+function Add_numbers_1() {
+    var X = 10;
+    console.log(15 + X);
+}
+function Add_numbers_2() {
+    console.log(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
---
+++ JS/local variable.js
@@ -0,0 +1,9 @@
+function Add_numbers_1() {
+    var X = 10;
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_2() {
+    document.write(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
