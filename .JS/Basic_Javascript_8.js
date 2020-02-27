3a50ee975ebeed55ede5fd50b350d8c7750d542d
ysaechao30 <ysaechao30@gmail.com>
2020-02-26 19:39:13 -0800

    updated
    Basic_HTML_8.html should be functoning now.

---
+++ .JS/Basic_Javascript_8.js
@@ -0,0 +1,5 @@
+function slice_Method() {
+    var Sentence = "I am trying to pretend I know what I'm doing";
+    var Section = Sentence.slice(15,22);
+    document.getElementById("Slice") .innerHTML = Section;
+}
