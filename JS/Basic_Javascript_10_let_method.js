3440121b9bcc3277ac463385b15479d5627bc889
ysaechao30 <ysaechao30@gmail.com>
2020-04-11 15:08:35 -0700

    update
    added and updated "object" assignment.

--- JS/Basic_Javascript_10_let_method.js
+++ JS/Basic_Javascript_10_let_method.js
@@ -3,9 +3,9 @@ let car = {
     model: "Viper ",
     year: "2021 ",
     color: "red ",
-    description: function() {
+    description : function() {
         return "The car is a " + this.year + this.color + this.make + this.model;
     }
-
+    
 };
 document.getElementById("Car_Object").innerHTML = car.description();
